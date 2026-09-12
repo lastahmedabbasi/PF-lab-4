47e7891452195a499ff3bbc41e2b2ea723ab2538
lastahmedabbasi <ahmedabbasiyt@gmail.com>
2026-09-12 21:20:18 +0500

    Update task1.c

--- task1.c
+++ task1.c
@@ -11,8 +11,19 @@ int main() {
 		largest = b;
 	if (c > largest)
 		largest = c;
+	
+	if (a == largest && b == largest && c == largest)
+		printf("All three numbers are equal and greatest: %d\n", largest);
+	else if ((a == largest && b == largest) ||
+			 (a == largest && c == largest) ||
+			 (b == largest && c == largest))
+		printf("Two numbers are equal and greatest: %d\n", largest);
+	else
+		printf("The largest number is %d\n", largest);
 
-	printf("The largest number is %d\n", largest);
+	return 0;
+}
+
 
 	return 0;
 }
